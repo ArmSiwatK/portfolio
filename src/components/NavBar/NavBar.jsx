2841a254aba386e03abaae1dde5ebd61d6a4d7c5
ArmSiwatK <arm.siwatk@gmail.com>
2023-05-18 18:37:14 +0700

    Responsive top name

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './NavBar.css';
 
 const NavBar = () => {
+    const [screenSize, setScreenSize] = useState('');
+
+    useEffect(() => {
+        const handleResize = () => {
+            const width = window.innerWidth;
+            if (width >= 1100) {
+                setScreenSize('Siwat “Arm” Kanjanapanitkun');
+            } else if (width >= 576) {
+                setScreenSize('Siwat K. (Arm)');
+            } else {
+                setScreenSize('Arm');
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
+        handleResize(); // Set initial screen size
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <nav className="nav">
-            <div className="logo">Siwat “Arm” Kanjanapanitkun</div>
+            <div className="logo">{screenSize}</div>
         </nav>
     );
 };
