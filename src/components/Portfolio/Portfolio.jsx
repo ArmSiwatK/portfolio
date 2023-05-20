3827a0977b6fb8bbd97f3936034665bfc0315768
ArmSiwatK <arm.siwatk@gmail.com>
2023-05-20 19:22:34 +0700

    Two more for portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -7,14 +7,26 @@ const Portfolio = () => {
         <div className="portfolio">
             <PortfolioItem
                 title="Character Selection Mockup"
-                gifSrc="/images/demo1.gif"
+                gifSrc="/images/demos/demo1.gif"
                 appLink="https://three-character-selection.vercel.app/"
                 repoLink="https://github.com/ArmSiwatK/Three-Character-Selection"
             />
             <PortfolioItem
                 title="Find Your Hat Challenge Project"
-                gifSrc="/images/demo2.gif"
+                gifSrc="/images/demos/demo2.gif"
                 appLink="https://replit.com/@SiwatArm/JavaScript-Find-My-Hat"
+                repoLink=""
+            />
+            <PortfolioItem
+                title="Play Multiple YouTube Videos"
+                gifSrc="/images/demos/demo3.gif"
+                appLink="https://play-multiple-videos.vercel.app/"
+                repoLink="https://github.com/ArmSiwatK/play-multiple-videos"
+            />
+            <PortfolioItem
+                title="Bad Apple!! but they're Minecraft"
+                gifSrc="/images/demos/demo4.gif"
+                appLink="https://youtu.be/n86O1YcHOuU"
                 repoLink=""
             />
         </div>
