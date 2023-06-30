344109b3646eff8d3dedc57584270b6372b9628a
ArmSiwatK <arm.siwatk@gmail.com>
2023-06-30 16:32:42 +0700

    update: Add Character Gallery project

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -12,26 +12,32 @@ const Portfolio = () => {
                 repoLink="https://github.com/ArmSiwatK/Three-Character-Selection"
             />
             <PortfolioItem
+                title="Character Gallery"
+                gifSrc="/images/demos/demo2.gif"
+                appLink="https://touhou-characters.vercel.app/"
+                repoLink="https://github.com/ArmSiwatK/touhou-characters"
+            />
+            <PortfolioItem
                 title="Orange Cat CRUD Web App"
-                gifSrc="/images/demos/demo5.gif"
+                gifSrc="/images/demos/demo6.gif"
                 appLink="https://orangecat.vercel.app/"
                 repoLink="https://github.com/lethal1147/frontend-jsd4-groupE"
             />
             <PortfolioItem
                 title="Find Your Hat Challenge Project"
-                gifSrc="/images/demos/demo2.gif"
+                gifSrc="/images/demos/demo3.gif"
                 appLink="https://replit.com/@SiwatArm/JavaScript-Find-My-Hat"
                 repoLink=""
             />
             <PortfolioItem
                 title="Play Multiple YouTube Videos"
-                gifSrc="/images/demos/demo3.gif"
+                gifSrc="/images/demos/demo4.gif"
                 appLink="https://play-multiple-videos.vercel.app/"
                 repoLink="https://github.com/ArmSiwatK/play-multiple-videos"
             />
             <PortfolioItem
                 title="Bad Apple!! but they're Minecraft"
-                gifSrc="/images/demos/demo4.gif"
+                gifSrc="/images/demos/demo5.gif"
                 appLink="https://youtu.be/n86O1YcHOuU"
                 repoLink=""
             />
